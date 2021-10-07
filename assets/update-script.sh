b776b02f706c804b88fcb8a42b10e7c35d5264af
tombetthauser <tombetthauser@gmail.com>
2021-10-07 11:14:02 -0700

    Update this repo

--- assets/update-script.sh
+++ assets/update-script.sh
@@ -1,1 +1,1 @@
-while true; do cp index.html README.md; git add -A; git commit -m 'Update this repo'; git push; sleep 900; done
+while true; do cp ../index.html ../README.md; git add -A; git commit -m 'Update this repo'; git push; sleep 900; done
