0f34cf0f623a9ab6f588f3e23c0dd15a2beeabed
tombetthauser <tombetthauser@gmail.com>
2021-10-27 09:57:21 -0700

    Update this repo

--- assets/update-script.sh
+++ assets/update-script.sh
@@ -1,3 +1,3 @@
-while true; do git add -A; git commit -m 'Update this repo'; git push; sleep 900; done
+while true; do git add -A; git commit -m 'Update this repo'; git push; sleep 200; done
 # while true; do cp ../index.html ../README.md; git add -A; git commit -m 'Update this repo'; git push; sleep 900; done
 # while true; do cp ../index.html ../README.md; git add -A; git commit -m 'Update this repo'; git push; sleep 900; done
